956bfa661a34d7b171159f36e2e9a97a7ee12685
John Arnold <iohannes.eduardus.arnold+code@gmail.com>
2016-11-20 04:46:09 +0000

    Alphabetize script

---
+++ lat/alphabetize.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+from lxml import etree
+import string
+import sys
+
+parser = etree.XMLParser(remove_blank_text=True, no_network=False)
+doc = etree.parse(sys.argv[1], parser)
+
+for letter in string.ascii_uppercase:
+    path = "//div0[@n='%s']" % letter
+    printDoc = open('ls_'+letter+'.xml', 'w')
+    for i in doc.xpath(path):
+        printDoc.write(etree.tounicode(i, pretty_print=True))
+    printDoc.close()
